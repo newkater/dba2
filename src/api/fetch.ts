25f8e35056ec7c4027ccb0dafa11ec4930e242a0
newkater <kate.korobeinikova@gmail.com>
2022-11-19 17:16:51 +0600

    fetch

---
+++ src/api/fetch.ts
@@ -0,0 +1,65 @@
+const parseResponse = <TResponse>(text: string) => {
+    return (text.length > 0 ? JSON.parse(text) : {}) as TResponse;
+}
+
+export const Get = async <TResponse>(url: string): Promise<TResponse> => {
+    const options: RequestInit = {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"},
+        redirect: "follow",
+        referrerPolicy: "no-referrer"
+    };
+
+    const response = await fetch(url, options);
+    return parseResponse<TResponse>(await response.text());
+}
+
+export const Put = async <TRequest, TResponse>(url: string, data: TRequest): Promise<TResponse> => {
+    const options: RequestInit = {
+        method: "PUT",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"},
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data)
+    };
+
+    const response = await fetch(url, options);
+    return parseResponse<TResponse>(await response.text());
+}
+
+export const Post = async <TRequest, TResponse>(url: string, data: TRequest): Promise<TResponse> => {
+    const options: RequestInit = {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"},
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data)
+    };
+
+    const response = await fetch(url, options);
+    return parseResponse<TResponse>(await response.text());
+}
+
+export const Delete = async <TResponse>(url: string): Promise<TResponse> => {
+    const options: RequestInit = {
+        method: "DELETE",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "include",
+        headers: {"Content-Type": "application/json"},
+        redirect: "follow",
+        referrerPolicy: "no-referrer"
+    };
+
+    const response = await fetch(url, options);
+    return parseResponse<TResponse>(await response.text());
+}
