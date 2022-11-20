4139fd79c5c122efdd99f5250b0643cad810547e
newkater <kate.korobeinikova@gmail.com>
2022-11-20 18:51:40 +0600

    discover create page

--- src/pages/discovers/DiscoverCreatePage.tsx
+++ src/pages/discovers/DiscoverCreatePage.tsx
@@ -1,7 +1,11 @@
 import {FC} from "react";
+import {DiscoverForm} from "../../components/discovers/discover-form/DiscoverForm";
+import {Discover} from "../../models/Discover";
 
 export const DiscoverCreatePage: FC = () => {
+    const discover:Discover = {cname: "", disease_code: "", first_enc_date: new Date()};
     return (<>
         <h2>Discover Create Page</h2>
+        <DiscoverForm discover={discover} formType="create"></DiscoverForm>
     </>)
 };
